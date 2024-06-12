a59ef734fa590fd3b10af66e75c41b695d526486
PrateekTechmatys <prateeky562@gmail.com>
2024-06-13 00:30:29 +0530

    setUp firebase

---
+++ frontend/src/components/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcLkrsD9-xAk33J9s1I3KOUNlDsEuVKqc",
+  authDomain: "codepan-440e7.firebaseapp.com",
+  projectId: "codepan-440e7",
+  storageBucket: "codepan-440e7.appspot.com",
+  messagingSenderId: "329521128613",
+  appId: "1:329521128613:web:415091b41749d375d0994c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
