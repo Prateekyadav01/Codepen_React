c84d411f3d54e635dce48a44f01dc00b40da3c1f
PrateekTechmatys <prateeky562@gmail.com>
2024-06-13 09:51:12 +0530

    home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,12 +3,16 @@ import { useSelector } from 'react-redux';
 
 const Home = () => {
     const results = useSelector((state) => state.result.results);
-
+    const handleDelete =(index)=>{
+        return ()=>{
+            console.log(index)
+        }
+    }
     return (
         <div>
             <h2 className="text-white">Stored Results</h2>
             {results.map((result, index) => (
-                <div key={index} className="bg-gray-800 p-4 mt-4 rounded-lg">
+                <div key={index} className="bg-gray-800 p-4 mt-4 rounded-lg w-fit">
                     <iframe
                         className="w-full h-60 border border-gray-700 rounded-md"
                         srcDoc={result}
@@ -17,6 +21,9 @@ const Home = () => {
                         width="100%"
                         height="100%"
                     ></iframe>
+                    <button onClick={handleDelete(index)}>
+                        Delete
+                    </button>
                 </div>
             ))}
         </div>
--- frontend/src/components/code/NavbarCode.jsx
+++ frontend/src/components/code/NavbarCode.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import images from '../../images/codepen.png'
+import { useNavigate } from 'react-router-dom'
 const NavbarCode = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate('/');
+  }
   return (
     <div
     className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
     <div className="left">
-        <div className="logo font-bold text-2xl text-white text-center">
+        <div onClick={handleBack} className="logo font-bold text-2xl text-white text-center">
             <img className=' w-44' src={images} alt="" />
         </div>
     </div>
